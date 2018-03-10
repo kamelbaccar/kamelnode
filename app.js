8601123fec6ab52f1bfaef810c7fd0fd2a34c989
kamel <36617686+kamelbaccar@users.noreply.github.com>
2018-03-10 23:15:41 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+
+app.get('/Myapp',function(req,res){
+  res.sendFile(path.join(__dirname+'/Myapp/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/page.html'));
+});
+
+app.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname+'/page.html'));
+});
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
