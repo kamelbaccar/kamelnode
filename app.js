b678d70f6c324b9d6bdb2cc1774610dda07b1bd7
kamel <36617686+kamelbaccar@users.noreply.github.com>
2018-03-11 11:53:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
-var express = require("express");
-var app     = express();
-var path    = require("path");
+var http = require('http');
 
-
-app.get('/Myapp',function(req,res){
-  res.sendFile(path.join(__dirname+'/Myapp/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
-app.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/page.html'));
-});
-
-app.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname+'/page.html'));
-});
-
-app.listen(8080);
-
-console.log("Running at Port 3000");
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(8080);
